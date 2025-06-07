831aa537e8e549a60654a0b48ce976d5b9c12da7
Victor Morales <chipahuac@hotmail.com>
2025-06-06 20:41:31 -0700

    Separate parser logic

--- src/mcp_proxy/mcp.py
+++ src/mcp_proxy/mcp.py
@@ -21,120 +21,10 @@ from urllib.parse import urlparse
 
 from mcp.server.fastmcp import FastMCP
 from openapi_parser import parse
-from openapi_parser.enumeration import DataType
-from openapi_parser.specification import AnyOf, Operation, Schema
+
+from mcp_proxy.parser import get_function_template
 
 logger = logging.getLogger()
-
-# OpenAPI 3.0 Data Types mapping
-# (https://swagger.io/docs/specification/v3_0/data-models/data-types/)
-data_type = {
-    DataType.STRING: "str",
-    DataType.INTEGER: "int",
-    DataType.NUMBER: "float",
-    DataType.BOOLEAN: "bool",
-    DataType.OBJECT: "dict",
-    DataType.ARRAY: "list",
-    DataType.NULL: "None",
-}
-
-
-def _get_function_template(url_path: str, operation: Operation) -> str:
-    inputs = _get_inputs(operation)
-    func_name = operation.operation_id
-
-    params = ", ".join(
-        [f"{input.name}: {input.type}{input.default}" for input in inputs]
-    )
-    params_docstring = ""
-    if len(inputs) > 0:
-        params_docstring = (
-            "\n    Parameters:\n"
-            + "\n".join(
-                [
-                    f"        {input.name} ({input.type}): {input.title}"
-                    for input in inputs
-                ]
-            )
-            + "\n"
-        )
-    data_template = "{%s}" % ", ".join(
-        [f"'{input.name}': {input.name}" for input in inputs]
-    )
-    return f"""def {func_name}({params}) -> dict:
-    '''
-    {operation.summary}
-    {params_docstring}
-    Returns:
-        dict
-    '''
-    import requests
-
-    data={data_template}
-    response = requests.request('{operation.method.name}', {url_path}, json=data)
-
-    return response.json()
-    """
-
-
-class Input:
-
-    def __init__(self, name: str, schema: Schema, required: bool = True):
-        self._name = name
-        if isinstance(schema, AnyOf):
-            self._type = "|".join(
-                [self._get_type(s.type, required) for s in schema.schemas]
-            )
-        else:
-            self._type = self._get_type(schema.type, required)
-        self._default = schema.default
-        self._title = schema.title
-
-    def _get_type(self, type: DataType, required: bool = True) -> str:
-        return (
-            data_type.get(type, "") if required else "%s|None" % data_type.get(type, "")
-        )
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def type(self) -> str:
-        return self._type
-
-    @property
-    def title(self) -> str:
-        return self._title
-
-    @property
-    def default(self) -> str:
-        if self._default is not None:
-            if str(self._default) == "":
-                return " = ''"
-            return " = %s" % self._default
-        return ""
-
-
-def _get_inputs(operation: Operation) -> list[Input]:
-    inputs = [
-        Input(param.name, param.schema, param.required)
-        for param in operation.parameters
-    ]
-    if (
-        hasattr(operation, "request_body")
-        and operation.request_body
-        and operation.request_body.required
-    ):
-        for content in operation.request_body.content:
-            if content.schema.required:
-                inputs.extend(
-                    [
-                        Input(prop.name, prop.schema)
-                        for prop in content.schema.properties
-                    ]
-                )
-    return inputs
 
 
 class Server:
@@ -162,7 +52,7 @@ class Server:
                 if skip_tool and operation.operation_id in skip_tool:
                     continue
                 func_name = operation.operation_id
-                func_template = _get_function_template(
+                func_template = get_function_template(
                     f'f"{base_url}{path.url}"', operation
                 )
                 new_func = compile(func_template, "<string>", "exec")
---
+++ src/mcp_proxy/parser.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2025
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module the converts OpenAPI types to Python code."""
+
+import logging
+
+from openapi_parser.enumeration import DataType
+from openapi_parser.specification import AnyOf, Operation, Schema
+
+logger = logging.getLogger()
+
+# OpenAPI 3.0 Data Types mapping
+# (https://swagger.io/docs/specification/v3_0/data-models/data-types/)
+data_type = {
+    DataType.STRING: "str",
+    DataType.INTEGER: "int",
+    DataType.NUMBER: "float",
+    DataType.BOOLEAN: "bool",
+    DataType.OBJECT: "dict",
+    DataType.ARRAY: "list",
+    DataType.NULL: "None",
+}
+
+
+def get_function_template(url_path: str, operation: Operation) -> str:
+    inputs = _get_inputs(operation)
+    func_name = operation.operation_id
+
+    params = ", ".join(
+        [f"{input.name}: {input.type}{input.default}" for input in inputs]
+    )
+    params_docstring = ""
+    if len(inputs) > 0:
+        params_docstring = (
+            "\n    Parameters:\n"
+            + "\n".join(
+                [
+                    f"        {input.name} ({input.type}): {input.title}"
+                    for input in inputs
+                ]
+            )
+            + "\n"
+        )
+    data_template = "{%s}" % ", ".join(
+        [f"'{input.name}': {input.name}" for input in inputs]
+    )
+    return f"""def {func_name}({params}) -> dict:
+    '''
+    {operation.summary}
+    {params_docstring}
+    Returns:
+        dict
+    '''
+    import requests
+
+    data={data_template}
+    response = requests.request('{operation.method.name}', {url_path}, json=data)
+
+    return response.json()
+    """
+
+
+class Input:
+    def __init__(self, name: str, schema: Schema, required: bool = True):
+        self._name = name
+        if isinstance(schema, AnyOf):
+            self._type = "|".join(
+                [self._get_type(s.type, required) for s in schema.schemas]
+            )
+        else:
+            self._type = self._get_type(schema.type, required)
+        self._default = schema.default
+        self._title = schema.title
+
+    def _get_type(self, type: DataType, required: bool = True) -> str:
+        return (
+            data_type.get(type, "") if required else "%s|None" % data_type.get(type, "")
+        )
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def default(self) -> str:
+        if self._default is not None:
+            if str(self._default) == "":
+                return " = ''"
+            return " = %s" % self._default
+        return ""
+
+
+def _get_inputs(operation: Operation) -> list[Input]:
+    inputs = [
+        Input(param.name, param.schema, param.required)
+        for param in operation.parameters
+    ]
+    if (
+        hasattr(operation, "request_body")
+        and operation.request_body
+        and operation.request_body.required
+    ):
+        for content in operation.request_body.content:
+            if content.schema.required:
+                inputs.extend(
+                    [
+                        Input(prop.name, prop.schema)
+                        for prop in content.schema.properties
+                    ]
+                )
+    return inputs
